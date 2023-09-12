fd28b624603ee9b760a558ac10b080ae3ddb5960
kangealme <kangealme@gmail.com>
2023-09-12 11:58:28 +0700

    Menambahkan file home, about,dan post untuk mencoba route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css"/>
+    <title>Kangealme | About</title>
+</head>
+
+<body>
+    <h1>Halaman About</h1>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" width="200" alt="kangealme">
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "kangealme",
+        "email" => "kangealme@gmail.com",
+        "image" => "kangealme.png"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
